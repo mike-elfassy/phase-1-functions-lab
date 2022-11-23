761052c1febcae74f91f47fe4bcd7d9f99e955dd
Michael Elfassy <mike.elfassy@gmail.com>
2022-11-23 15:51:10 -0500

    commit tree dbb179ee857d89bcd2d8fc021496df1c72b575fc

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+const distanceFromHqInBlocks = function(pickupLocation) {
+    return Math.abs(42 - pickupLocation);
+};
+
+const distanceFromHqInFeet = function(pickupLocation) {
+    return distanceFromHqInBlocks(pickupLocation) * 264;
+};
+
+const distanceTravelledInFeet = function(startBlock, endBlock) {
+    return Math.abs(startBlock - endBlock) * 264;
+};
+
+const calculatesFarePrice = function(startBlock, endBlock) {
+    let distance = distanceTravelledInFeet(startBlock, endBlock);
+    let fare;
+    switch (true) {
+        case distance <= 400:
+            fare = 0;
+            break;
+        case distance < 2000:
+            fare = (distance - 400) * 0.02;
+            break;
+        case distance < 2500:
+            fare = 25;
+            break;
+        default:
+            fare = 'cannot travel that far';
+            break;
+    };
+    return fare;
+};
